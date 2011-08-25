bda769661f80721889f261587927e8d7e6b79374
Petar Tomičić <Petar Tomii@5AR>
2011-08-25 11:11:13 +0200

    First Line of the Text
    tralala
    
    Signed-off-by: Joe

--- HelloWorld/src/HelloWorldTest.java
+++ HelloWorld/src/HelloWorldTest.java
@@ -7,6 +7,7 @@ public class HelloWorldTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		System.out.println("Second World Hello");
 	}
 
 }
